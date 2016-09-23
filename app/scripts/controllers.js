478f0e10832b3b941e319c16930c4d3fa8bf4a80
rocjrocj <roc@rocjohnson.com>
2016-09-23 09:51:57 -0500

    Fri 9-23 commit
    Fri 9-23 commit

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,6 +7,6 @@ var nonoControllers = angular.module('nonoControllers', []);
 nonoControllers.controller('nonoJsonController', ['$scope', '$http', function ($scope, $http) {
     $http.get("nono.json").then(function(response) {
         $scope.myData = response.data.strips;
-		console.log("$scope.myData: " + $scope.myData);
+		console.log("$scope.myData: ", $scope.myData);
     });
 }]);
